1f4d5246f8c2d73ca245cc6fbb39ef8a610af81f
OlyaBorzynska <mykytlo@gmail.com>
2026-03-14 18:37:56 +0200

    update js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,13 +1,8 @@
-// У файлі render-functions.js створи екземпляр SimpleLightbox для роботи з модальним вікном та
-// зберігай функції для відображення елементів інтерфейсу:
-
-// createGallery(images). Ця функція повинна приймати масив images, створювати HTML-розмітку для галереї,
-// додавати її в контейнер галереї та викликати метод екземпляра SimpleLightbox refresh(). Нічого не повертає.
-
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
 
 let simpleGallery = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
@@ -45,9 +40,13 @@ export function createGallery(images) {
 }
 
 export function clearGallery() {
-  gallery.outerHTML('');
+  gallery.innerHTML = '';
 }
 
-// clearGallery(). Ця функція нічого не приймає та повинна очищати вміст контейнера галереї. Нічого не повертає.
-// showLoader(). Ця функція нічого не приймає, повинна додавати клас для відображення лоадера. Нічого не повертає.
-// hideLoader(). Ця функція нічого не приймає, повинна прибирати клас для відображення лоадера. Нічого не повертає.
+export function showLoader() {
+  loader.classList.remove('hidden');
+}
+
+export function hideLoader() {
+  loader.classList.add('hidden');
+}
--- src/main.js
+++ src/main.js
@@ -2,34 +2,54 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 import { getImagesByQuery } from './js/pixabay-api';
-import { createGallery, clearGallery } from './js/render-functions';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions';
 
 const form = document.querySelector('.form');
-// const gallery = document.querySelector('.gallery');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-
+  clearGallery();
   const formData = new FormData(form);
   const textImage = formData.get('search-text');
 
   if (textImage.trim() === '') {
-    iziToast.warning({
-      message: `Fill in the search field`,
+    iziToast.error({
+      theme: 'dark',
+      position: 'topRight',
+      backgroundColor: 'red',
+      message: `Sorry, there are no images matching 
+      your search query. <br> Please try again!`,
     });
   } else {
-    getImagesByQuery(textImage.trim()).then(response => {
-      if (response.data.hits.length === 0) {
-        iziToast.warning({
-          message: `Sorry, there are no images matching your search query. Please try again!`,
+    showLoader();
+    getImagesByQuery(textImage.trim())
+      .then(response => {
+        if (response.data.hits.length === 0) {
+          iziToast.error({
+            theme: 'dark',
+            position: 'topRight',
+            backgroundColor: 'red',
+            message: `Sorry, there are no images matching 
+            your search query. <br> Please try again!`,
+          });
+        } else {
+          const markup = createGallery(response.data.hits);
+          form.reset();
+          hideLoader();
+        }
+      })
+      .catch(error => {
+        iziToast.error({
+          theme: 'dark',
+          position: 'topRight',
+          backgroundColor: 'red',
+          message: `ERROR: ${error}`,
         });
-      } else {
-        // clearGallery();
-
-        const markup = createGallery(response.data.hits);
-        form.reset();
-      }
-    });
-    //   .catch(error=>console.log(error);
+      });
   }
 });
